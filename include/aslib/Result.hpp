b57c730e4a64c0c0fd6ef112d0eea2b41b208a44
AmaiSaeta <amaisaeta@gmail.com>
2011-10-26 18:27:16 +0900

    aslib::Result実装

---
+++ include/aslib/Result.hpp
@@ -0,0 +1,275 @@
+﻿/**
+ * @file
+ */
+#ifndef ASLIB_INCLUDED_RESULT_HPP_
+#define ASLIB_INCLUDED_RESULT_HPP_
+
+#include <stdexcept>
+#include <type_traits>
+
+namespace aslib {
+
+/**
+ * 正常値とエラー値の両方を表現する型. 関数の戻り値として用いる.
+ * 通常、関数は決まった型の値しか返せず、関数の処理結果の値と、呼出元へのエラー発生通知を同時に行う為には、どちらかを(ポインタや参照型で)引数に持たなければならない。
+ * このクラスを用いる事により、処理結果とエラー通知のいずれかを関数の戻り値のみで表現出来る。
+ * @code
+ *	aslib::Result<Succeeded, Failed> function() {	// 正常に処理出来た場合は結果をSucceeded型で、エラーが発生した時はその内容をFailed型で返す関数
+ *		if(isSucceess) return Succceeded();
+ *		else if(isFailure) return Failed();
+ * 	}
+ *
+ *	aslib::Result<Succeeded, Failed> result = function();
+ *
+ *	if(result) {}	// 正常値かエラー値かはboolへの変換で判断出来る. 正常値であればtrue.
+ *
+ *	// dereference、或いは->演算子で正常値にアクセス出来る. エラー値であった場合は、aslib::Result::CantDereference例外が発生する.
+ *  Succeeded s = *result;
+ *  result->member();
+ *
+ *	// エラー値の値は、failedメンバ関数でアクセス出来る.
+ *	result = Failed();
+ *  if(!result) { result.failed().memberOfFailed(); }
+ * @endcode
+ * @param[in]	S	正常値の型.
+ * @param[in]	F	エラー値の型.
+ * @warning	現在のこのクラスでは、テンプレートパラメータとして参照を用いる事は出来ない.
+ * @sa	aslib::Result::CantDereference
+ */
+template<typename S, typename F>
+class Result {
+public:	// public types {{{
+	/// 正常値の型.
+	typedef S SucceededType;
+	/// エラー値の型.
+	typedef F FailedType;
+
+	/// エラー値を保持したaslib::Resultオブジェクトをdereference、または->演算子を利用した場合に発生する例外.
+	struct CantDereference : std::runtime_error {
+		CantDereference() : runtime_error("Failed result value.") {}
+	};
+	/// エラー値を保持していない(正常値或いは未初期化である)事を表す例外.
+	struct NotHaveFailedObject : std::runtime_error {
+		NotHaveFailedObject() : runtime_error("Not have the Failed object.") {}
+	};
+// }}}
+
+private: // private types {{{
+	typedef Result<SucceededType, FailedType> My;
+
+	/// 内部保持している値の種類.
+	enum State {
+		uninit,	///< 未初期化. 内部に保持している値は無い.
+		succeeded,	///< 正常値.
+		failed	///< エラー値.
+	};
+// }}}
+
+public:	// constructors / destructor {{{
+	Result() : state_(uninit) {}
+	/// @param[in]	src	正常値.
+	Result(const SucceededType &src) { allocate(&src,succeeded); }
+	/// @param[in]	src	正常値.
+	Result(SucceededType &&src) { allocate_move(&src, succeeded); }
+	/// @param[in]	src	エラー値.
+	Result(const FailedType &src) { allocate(&src, failed); }
+	/// @param[in]	src	エラー値.
+	Result(FailedType &&src) { allocate_move(&src, failed); }
+
+	/// @param[in]	src	コピー元.
+	Result(const Result &src) {
+		if(src.state_ == uninit) return;
+
+		allocate(src.storage_, src.state_);
+	}
+	/// @param[in]	src	コピー元.
+	Result(Result &&src) : state_(uninit) {
+		if(src.state_ == uninit) return;
+
+		allocate_move(&src.storage_, src.state_);
+	}
+
+	~Result() { destroy(); }
+// }}}
+
+public:	// functions / operators
+	/// @return	保持している値が正常値かエラー値か. trueならば正常値.
+	operator bool() const { return state_ == succeeded; }
+
+	/**
+	 * @return	正常値を保持している場合、その値への参照を返す.
+	 * @exception	aslib::Result::CantDereference	エラー値を保持している場合に発生する.
+	 */
+	SucceededType &operator*() { return const_cast<SucceededType &>(const_cast<const My *>(this)->operator*()); }
+	/// @overload
+	const SucceededType &operator*() const {
+		if(!operator bool()) { throw Result::CantDereference(); }
+		return *reinterpret_cast<const SucceededType *>(storage_);
+	}
+
+	/**
+	 * @return	正常値を保持している場合、その値へのポインタを返す.
+	 * @exception	aslib::Result::CantDereference	エラー値を保持している場合に発生する.
+	 */
+	SucceededType *operator->() { return const_cast<SucceededType *>(const_cast<const My *>(this)->operator->()); }
+	/// @overload
+	const SucceededType *operator->() const {
+		if(!operator bool()) { throw Result::CantDereference(); }
+		return reinterpret_cast<const SucceededType *>(storage_);
+	}
+
+	/**
+	 * @return	エラー値を保持している場合に、その値への参照を返す.
+	 */
+	FailedType &fail() { return const_cast<FailedType &>(const_cast<const My *>(this)->fail()); }
+	// @overload
+	const FailedType &fail() const {
+		if(state_ == failed) return *reinterpret_cast<const FailedType *>(storage_);
+		throw NotHaveFailedObject();
+	}
+
+	Result &operator=(const SucceededType &rhs) { return substitute(&rhs, succeeded); }
+	Result &operator=(SucceededType &&rhs) { return substitute_move(&rhs, succeeded); }
+	Result &operator=(const FailedType &rhs) { return substitute(&rhs, failed); }
+	Result &operator=(FailedType &&rhs) { return substitute_move(&rhs, failed); }
+	Result &operator=(const Result &rhs) { return substitute(rhs.storage_, rhs.state_); }
+	Result &operator=(Result &&rhs) { return substitute_move(rhs.storage_, rhs.state_); }
+// }}}
+
+private:	// inner functions. {{{
+	/**
+	 * 指定した型の値を構築する.
+	 * 格納型のコピーコンストラクタを使用する.
+	 * @param[in]	src	コピー元
+	 * @param[in]	state	コピー元の値種別.
+	 */
+	void allocate(const void *src, State srcState) {
+		switch(srcState) {
+		case succeeded:
+			new(storage_) SucceededType(*static_cast<const SucceededType *>(src));
+			break;
+		case failed:
+			new(storage_) FailedType(*static_cast<const FailedType *>(src));
+			break;
+		}
+		state_ = srcState;
+	}
+	/**
+	 * @overload
+	 * 指定した型の値を構築する.
+	 * 格納型のデフォルトコンストラクタを使用する.
+	 * @param[in]	state	
+	 */
+	void allocate(State state) {
+		assert(state != uninit);
+
+		switch(state) {
+		case succeeded: new(storage_) SucceededType(); break;
+		case failed: new(storage_) FailedType(); break;
+		}
+		state_ = state;
+	}
+
+	/**
+	 * 指定した型の値を構築する.
+	 * 格納型のムーブコンストラクタを使用する.
+	 * @param[in]	src	コピー元
+	 * @param[in]	state	コピー元の値種別.
+	 */
+	void allocate_move(void *src, State srcState) {
+		switch(srcState) {
+		case succeeded:
+			new(storage_) SucceededType(std::move(*static_cast<SucceededType *>(src)));
+			break;
+		case failed:
+			new(storage_) FailedType(std::move(*static_cast<FailedType *>(src)));
+			break;
+		}
+		state_ = srcState;
+	}
+
+	/**
+	 * 指定した型の値を代入する.
+	 * 格納型に対応した代入演算子を使用する.
+	 * @param[in]	src	代入元を指すポインタ.
+	 * @param[in]	srcState	代入元の種類.
+	 * @return	代入処理を行った自分自身(*this).
+	 */
+	Result &substitute(const void *src, State srcState) {
+		if(state_ != uninit) destroy();
+		if(srcState != uninit) allocate(srcState);
+		switch(srcState) {
+		case succeeded:
+			*reinterpret_cast<SucceededType *>(storage_) = *static_cast<const SucceededType *>(src);
+			break;
+		case failed:
+			*reinterpret_cast<FailedType *>(storage_) = *static_cast<const FailedType *>(src);
+			break;
+		}
+		state_ = srcState;
+
+		return *this;
+	}
+	/**
+	 * 指定した型の値を代入する.
+	 * 右辺値参照を右辺値とするoperator=を使用する.
+	 * @param[in]	src	代入元を指すポインタ.
+	 * @param[in]	srcState	代入元の種類.
+	 * @return	代入処理を行った自分自身(*this).
+	 */
+	Result &substitute_move(void *src, State srcState) {
+		if(state_ != uninit) destroy();
+		if(srcState != uninit) allocate(srcState);
+		switch(srcState) {
+		case succeeded:
+			*reinterpret_cast<SucceededType *>(storage_) = std::move(*static_cast<SucceededType *>(src));
+			break;
+		case failed:
+			*reinterpret_cast<FailedType *>(storage_) = std::move(*static_cast<FailedType *>(src));
+			break;
+		}
+		state_ = srcState;
+
+		return *this;
+	}
+
+	/// 内部に保持している値を破棄する.
+	void destroy() {
+		switch(state_) {
+		case succeeded: Destoroyer<SucceededType>()(storage_); break;
+		case failed:    Destoroyer<FailedType   >()(storage_); break;
+		}
+		state_ = uninit;
+	}
+	/// 実際に値の破棄を行う.
+	template<typename T>
+	struct Destoroyer {
+		void operator()(void *p) { destory<std::is_pod<T>::value>(p); }
+	private:
+		template<typename bool isPod /* == true */> void destory(void *p) { /* NOTHING */ }
+		template<> void destory<false>(void *p) { static_cast<T *>(p)->~T(); }
+	};
+
+	/**
+	 * 正常値、エラー値双方とも保持可能なサイズを算出するメタ関数.
+	 */
+	class CalcStorageSize {
+		static const std::size_t sSize = sizeof(SucceededType);
+		static const std::size_t fSize = sizeof(FailedType);
+		static const std::size_t cSize = sizeof(char);
+		static const std::size_t storageSize = (sSize > fSize) ? sSize : fSize;
+	public:
+		static const std::size_t value = storageSize / cSize + ((storageSize % cSize) > 0);
+	};
+// }}}
+
+private:	// fields {{{
+
+	char storage_[CalcStorageSize::value];
+	State state_;
+// }}}
+};
+
+}	// namespace aslib
+
+#endif	// ASLIB_INCLUDED_RESULT_HPP_
---
+++ test/ResultTest.cpp
@@ -0,0 +1,252 @@
+﻿#include <iostream>
+#include <typeinfo>
+
+std::ostream &operator<<(std::ostream &stream, const std::type_info &type) { return stream << type.name(); }
+
+#define BOOST_TEST_MAIN
+#include <boost/test/auto_unit_test.hpp>
+
+#include <aslib/Result.hpp>
+using aslib::Result;
+
+namespace {
+	enum CallFlag {
+		construct,
+		copyConstruct,
+		moveConstruct,
+		copyOpEp,
+		moveOpEp,
+		destruct
+	};
+	struct SucceessTypeTag {};
+	struct FailedTypeTag {};
+	template<typename kindTag>
+	struct CoType {
+		CoType() : flag_(nullptr) {}
+		CoType(CallFlag *flag) : flag_(flag) { *flag_ = construct; }
+		CoType(const CoType &src) : flag_(src.flag_) { if(flag_) *flag_ = copyConstruct; }
+		CoType(CoType &&src) : flag_(src.flag_) { if(flag_) *flag_ = moveConstruct; }
+		CoType &operator=(const CoType &rhs) { flag_ = rhs.flag_; if(flag_) *flag_ = copyOpEp; return *this; }
+		CoType &operator=(CoType &&rhs) { flag_ = rhs.flag_; if(flag_) *flag_ = moveOpEp; return *this; }
+		~CoType() { if(flag_) *flag_ = destruct; }
+		CallFlag *flag_;
+	};
+	typedef CoType<SucceessTypeTag> SucceededType;
+	typedef CoType<FailedTypeTag> FailedType;
+
+	typedef Result<SucceededType, FailedType> R;
+
+	struct Fixture {
+		Fixture()
+			: s(SucceededType(&s_flag)), f(FailedType(&f_flag)),
+			  cs(SucceededType(&cs_flag)), cf(FailedType(&cf_flag)) {}
+		R u,s,f;	// (non-const) uninitialized, in-succeeded, in-failed
+		const R cu, cs, cf;	// (const) uninitialized, in-succeeded, in-failed
+		CallFlag u_flag, s_flag, f_flag, cu_flag, cs_flag, cf_flag;
+	};
+}
+
+BOOST_FIXTURE_TEST_SUITE(ResultTest, Fixture)
+	BOOST_AUTO_TEST_CASE(testConstructor) {
+			SucceededType s;
+			FailedType f;
+			R ru, rs(s), rf(f);
+			const R cru, crs(s), crf(f);
+	}
+
+	BOOST_AUTO_TEST_CASE(testCopyConstructor) {
+		R sd(s), fd(f), ud(u);
+		BOOST_CHECK(static_cast<bool>(sd));
+		BOOST_CHECK(!static_cast<bool>(fd));
+		BOOST_CHECK(!static_cast<bool>(ud));
+		BOOST_CHECK_THROW(ud.fail(), R::NotHaveFailedObject);
+
+		const R csd(s), cfd(f), cud(u);
+		BOOST_CHECK(static_cast<bool>(csd));
+		BOOST_CHECK(!static_cast<bool>(cfd));
+		BOOST_CHECK(!static_cast<bool>(cud));
+		BOOST_CHECK_THROW(cud.fail(), R::NotHaveFailedObject);
+	}
+
+	BOOST_AUTO_TEST_CASE(testMoveConstructor) {
+		CallFlag s_flag, f_flag;
+		SucceededType st(&s_flag);
+		FailedType ft(&f_flag);
+		BOOST_REQUIRE_EQUAL(s_flag, construct);
+		BOOST_REQUIRE_EQUAL(f_flag, construct);
+
+		R rs(std::move(st));
+		R rf(std::move(ft));
+		BOOST_CHECK_EQUAL(s_flag, moveConstruct);
+		BOOST_CHECK_EQUAL(f_flag, moveConstruct);
+	}
+
+	BOOST_AUTO_TEST_CASE(testMoveConstructor_from_Result) {
+		// from Result(succeeded)
+		R sDst(std::move(s));
+		BOOST_CHECK_EQUAL(s_flag, moveConstruct);
+		// from Result(failed)
+		R fDst(std::move(f));
+		BOOST_CHECK_EQUAL(f_flag, moveConstruct);
+
+		// 未初期化のResultからのmove constructはチェックのしようがない……
+	}
+
+	BOOST_AUTO_TEST_CASE(testDestructor) {
+		CallFlag sflag, fflag;
+		{
+			SucceededType st(&sflag);
+			FailedType ft(&fflag);
+			R rs(st), rf(ft);
+			BOOST_REQUIRE_NE(sflag, destruct);
+			BOOST_REQUIRE_NE(fflag, destruct);
+		}
+		BOOST_CHECK_MESSAGE(sflag==destruct, "not destruct rs");
+		BOOST_CHECK_MESSAGE(fflag==destruct, "not destruct rf");
+	}
+
+	BOOST_AUTO_TEST_CASE(testOpBool) {
+		BOOST_CHECK(static_cast<bool>(s));
+		BOOST_CHECK(!static_cast<bool>(f));
+		BOOST_CHECK(!static_cast<bool>(u));
+
+		BOOST_CHECK(static_cast<bool>(cs));
+		BOOST_CHECK(!static_cast<bool>(cf));
+		BOOST_CHECK(!static_cast<bool>(cu));
+	}
+
+	BOOST_AUTO_TEST_CASE(testOpDereference) {	// operator *
+		BOOST_CHECK_EQUAL(typeid(*s), typeid(SucceededType&));
+		BOOST_CHECK_THROW(*f, R::CantDereference);
+		BOOST_CHECK_THROW(*u, R::CantDereference);
+
+		BOOST_CHECK_EQUAL(typeid(*cs), typeid(const SucceededType&));
+		BOOST_CHECK_THROW(*cf, R::CantDereference);
+		BOOST_CHECK_THROW(*cu, R::CantDereference);
+	}
+
+	BOOST_AUTO_TEST_CASE(testOpArrow) {	// operator->
+		BOOST_CHECK_EQUAL(typeid(s.operator->()), typeid(SucceededType*));
+		BOOST_CHECK_THROW(f.operator->(), R::CantDereference);
+		BOOST_CHECK_THROW(u.operator->(), R::CantDereference);
+
+		BOOST_CHECK_EQUAL(typeid(cs.operator->()), typeid(const SucceededType*));
+		BOOST_CHECK_THROW(cf.operator->(), R::CantDereference);
+		BOOST_CHECK_THROW(cu.operator->(), R::CantDereference);
+	}
+
+	BOOST_AUTO_TEST_CASE(testFail) {
+		BOOST_CHECK_THROW(s.fail(), R::NotHaveFailedObject);
+		BOOST_CHECK_EQUAL(typeid(f.fail()), typeid(FailedType&));
+		BOOST_CHECK_THROW(u.fail(), R::NotHaveFailedObject);
+
+		BOOST_CHECK_THROW(cs.fail(), R::NotHaveFailedObject);
+		BOOST_CHECK_EQUAL(typeid(cf.fail()), typeid(const FailedType&));
+		BOOST_CHECK_THROW(cu.fail(), R::NotHaveFailedObject);
+	}
+
+	BOOST_AUTO_TEST_CASE(testOpEp) {	// operator=
+		R r1, r2;
+		BOOST_REQUIRE(!static_cast<bool>(r1));
+		BOOST_REQUIRE(!static_cast<bool>(r2));
+
+		r1 = r2 = s;
+		BOOST_CHECK(static_cast<bool>(r1));
+		BOOST_CHECK(static_cast<bool>(r2));
+
+		r1 = r2 = f;
+		BOOST_CHECK(!static_cast<bool>(r1));
+		BOOST_CHECK_NO_THROW(r1.fail());
+		BOOST_CHECK(!static_cast<bool>(r2));
+		BOOST_CHECK_NO_THROW(r2.fail());
+
+		r1 = r2 = u;
+		BOOST_CHECK(!static_cast<bool>(r1));
+		BOOST_CHECK_THROW(r1.fail(), R::NotHaveFailedObject);
+		BOOST_CHECK(!static_cast<bool>(r2));
+		BOOST_CHECK_THROW(r2.fail(), R::NotHaveFailedObject);
+	}
+
+/*
+ Result::operator=(TYPE&&)用テストケース
+ TEST_CASE_OPEP_MOVE_(左辺値型種別, 右辺値型種別)
+   uninit	未初期化のResult
+   succeeded	正常値のResult
+   failed	エラー値のResult
+   co-succeeded	正常値
+   co-failed	エラー値
+*/
+#define TEST_CASE_OPEP_MOVE_(lhs, rhs) \
+	BOOST_AUTO_TEST_CASE(testOpEp_move_ ## lhs ## _ ## rhs) { \
+		TEST_CASE_OPEP_MOVE_LHS_ ## lhs; \
+		TEST_CASE_OPEP_MOVE_RHS_ ## rhs; \
+	}
+#define TEST_CASE_OPEP_MOVE_LHS_uninit    R r;
+#define TEST_CASE_OPEP_MOVE_LHS_succeeded SucceededType lst; R r(lst);
+#define TEST_CASE_OPEP_MOVE_LHS_failed    FailedType lft; R r(lft);
+
+#define TEST_CASE_OPEP_MOVE_RHS_uninit BOOST_FAIL("Can't runned test, because flag is not exist.");
+#define TEST_CASE_OPEP_MOVE_RHS_succeeded \
+	r = std::move(s); \
+	BOOST_CHECK_EQUAL(s_flag, moveOpEp);
+#define TEST_CASE_OPEP_MOVE_RHS_failed \
+	r = std::move(f); \
+	BOOST_CHECK_EQUAL(f_flag, moveOpEp);
+#define TEST_CASE_OPEP_MOVE_RHS_co_uninit TEST_CASE_OPEP_MOVE_RHS_uninit
+#define TEST_CASE_OPEP_MOVE_RHS_co_succeeded \
+	CallFlag flag; \
+	SucceededType rst(&flag); \
+	r = std::move(rst); \
+	BOOST_CHECK_EQUAL(flag, moveOpEp);
+#define TEST_CASE_OPEP_MOVE_RHS_co_failed \
+	CallFlag flag; \
+	FailedType rft(&flag); \
+	r = std::move(rft); \
+	BOOST_CHECK_EQUAL(flag, moveOpEp);
+
+	TEST_CASE_OPEP_MOVE_(uninit, succeeded);
+	TEST_CASE_OPEP_MOVE_(uninit, failed);
+	TEST_CASE_OPEP_MOVE_(uninit, co_succeeded);
+	TEST_CASE_OPEP_MOVE_(uninit, co_failed);
+	TEST_CASE_OPEP_MOVE_(succeeded, succeeded);
+	TEST_CASE_OPEP_MOVE_(succeeded, failed);
+	TEST_CASE_OPEP_MOVE_(succeeded, co_succeeded);
+	TEST_CASE_OPEP_MOVE_(succeeded, co_failed);
+	TEST_CASE_OPEP_MOVE_(failed, succeeded);
+	TEST_CASE_OPEP_MOVE_(failed, failed);
+	TEST_CASE_OPEP_MOVE_(failed, co_succeeded);
+	TEST_CASE_OPEP_MOVE_(failed, co_failed);
+	
+#undef TEST_CASE_OPEP_MOVE_
+#undef TEST_CASE_OPEP_MOVE_LHS_uninit
+#undef TEST_CASE_OPEP_MOVE_LHS_succeeded
+#undef TEST_CASE_OPEP_MOVE_LHS_failed
+#undef TEST_CASE_OPEP_MOVE_RHS_uninit
+#undef TEST_CASE_OPEP_MOVE_RHS_succeeded
+#undef TEST_CASE_OPEP_MOVE_RHS_failed
+#undef TEST_CASE_OPEP_MOVE_RHS_co_uninit
+#undef TEST_CASE_OPEP_MOVE_RHS_co_succeeded
+#undef TEST_CASE_OPEP_MOVE_RHS_co_failed
+
+	BOOST_AUTO_TEST_CASE(testCoTypedefs) {
+		typedef Result<SucceededType, FailedType> R;
+		BOOST_CHECK_EQUAL(typeid(R::SucceededType), typeid(SucceededType));
+		BOOST_CHECK_EQUAL(typeid(R::FailedType), typeid(FailedType));
+	}
+BOOST_AUTO_TEST_SUITE_END();
+
+BOOST_AUTO_TEST_SUITE(ResultCantDereferenceTest)
+	BOOST_AUTO_TEST_CASE(test) {
+		R::CantDereference e;	// 構築テスト
+		std::runtime_error &s = R::CantDereference();	// 基底クラス確認
+		e.what();	// 呼び出しテスト
+	}
+BOOST_AUTO_TEST_SUITE_END();
+
+BOOST_AUTO_TEST_SUITE(ResultNotHaveFailedObjectTest)
+	BOOST_AUTO_TEST_CASE(test) {
+		R::NotHaveFailedObject e;	// 構築テスト
+		std::runtime_error &s = R::NotHaveFailedObject();	// 基底クラス確認
+		e.what();	// 呼び出しテスト
+	}
+BOOST_AUTO_TEST_SUITE_END();
